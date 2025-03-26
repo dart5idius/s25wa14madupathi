4c652069f0fe50ae1b6cbd55baf4a8c45cbf9415
dart5idius <siddharth_95@rocketmail.com>
2025-03-26 14:11:24 -0500

    added mydata files

--- routes/mydata.js
+++ routes/mydata.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Siddharth madupathi' });
+router.get('/mydata', function(req, res, next) {
+  res.render('mydata', { title: 'Siddharth madupathi' });
 });
 
 module.exports = router;
