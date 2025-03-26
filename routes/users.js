7398caad3cee3aa55a2d6dc6e76d0a87f7145505
dart5idius <siddharth_95@rocketmail.com>
2025-03-26 18:53:43 -0500

    modified users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const ratio = 0.75;
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // Initialize acc if it doesn't exist
+  if (!req.app.locals.acc) {
+    req.app.locals.acc = 1000;
+  }
+
+  // Update the value
+  req.app.locals.acc = req.app.locals.acc * ratio;
+
+  // Send the response
+  res.send(`Value is: ${req.app.locals.acc}`);
 });
 
 module.exports = router;
